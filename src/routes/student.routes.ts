1d72c86c246e10d74179a71062f5b237cb7f508c
Juan David Velasquez <jdvr1994@hotmail.com>
2021-10-01 07:17:51 -0500

    Change student routes permissions

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -35,8 +35,8 @@ class StudentRouter{
         this.studentRouter.get('/:id', isAuth(authAll), this.instanceController.getStudent); // Get student by Id
         this.studentRouter.post('/', this.instanceController.createStudent); //Register student
         this.studentRouter.post('/login', this.instanceController.login); //Login (get Auth) for student
-        this.studentRouter.get('/:id/login-history', isAuth(authAll), this.instanceController.getLogginHistory);  //Get login history by studentId
-        this.studentRouter.put('/:id', isAuth(authAll), this.instanceController.updateStudent); //Update student
+        this.studentRouter.get('/:id/login-history', isAuth(authStudent), this.instanceController.getLogginHistory);  //Get login history by studentId
+        this.studentRouter.put('/:id', isAuth(authStudent), this.instanceController.updateStudent); //Update student
         this.studentRouter.delete('/:id', isAuth(authAdmin), this.instanceController.deleteStudent); //Update student
 
         return this.studentRouter;
